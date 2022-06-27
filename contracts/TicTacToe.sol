9114d950bb9bea529f8fd1cfd06c66b4b1c24c76
Fidexmr <fidexmr@dnmx.org>
2022-06-27 13:54:43 +0200

    Initial commit

---
+++ contracts/TicTacToe.sol
@@ -0,0 +1,37 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.22 <0.9.0;
+
+contract TicTacToe {
+
+  event Created(address host, uint bet);
+  event Started(address host);
+
+  struct Game {
+    address visitor;
+    uint balance;
+  }
+
+  mapping (address=>Game) games;
+
+  function create() public payable returns (uint balance){
+    require(msg.value > 0);
+    Game storage g = games[msg.sender];
+    g.balance = msg.value;
+    emit Created(msg.sender, g.balance);
+    return address(this).balance;
+  }
+
+  function join(address host) public payable {
+    Game storage g = games[host];
+    require(msg.value == g.balance);
+    g.balance += msg.value;
+    g.visitor = msg.sender;
+    emit Started(host);
+  }
+
+  function claim(address host) public {
+    require(games[host].balance > 0);
+    msg.sender.call{value: games[host].balance};
+    games[host].balance = 0;
+  }
+}
